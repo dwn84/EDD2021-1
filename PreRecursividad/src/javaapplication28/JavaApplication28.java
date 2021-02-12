a8c788566ceb719a020eb59ce41cbf42e64a0a35
dwn84 <dwn84@hotmail.com>
2021-02-12 08:51:44 -0500

    Actualización

--- PreRecursividad/src/javaapplication28/JavaApplication28.java
+++ PreRecursividad/src/javaapplication28/JavaApplication28.java
@@ -1,4 +1,4 @@
-/*
+﻿/*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
@@ -42,6 +42,7 @@ class pruebaRecursividad {
         if (x == 0) {
             System.out.println("Fin del proceso");
         } else {
+
             System.out.println(x);
             repetir(x - 1);
 
